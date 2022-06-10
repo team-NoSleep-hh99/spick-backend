c5c8f062cc2f8d534c366ff4568d003b1cc41e77
DKKwag <105155862+DKKwag@users.noreply.github.com>
2022-06-10 21:03:27 +0900

    첫첫 커밋

---
+++ spick/src/main/java/com/team/spick/SpickApplication.java
@@ -0,0 +1,13 @@
+package com.team.spick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpickApplication.class, args);
+    }
+
+}
---
+++ spick/src/test/java/com/team/spick/SpickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.spick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
